f8d2dea15d6e0b26c3ecd665c166b18170d9fe76
Christian LeVesque <christian@levesque.dev>
2024-09-02 10:40:34 -0500

    added component provider to contain references to configurable UI components

---
+++ src/Sienar.Utils.Blazor/Infrastructure/ComponentProvider.cs
@@ -0,0 +1,16 @@
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+using System;
+
+namespace Sienar.Infrastructure;
+
+/// <exclude />
+public class ComponentProvider : IComponentProvider
+{
+	public Type? DefaultLayout { get; set; }
+	public Type? Dashboard { get; set; }
+	public Type? AppbarLeft { get; set; }
+	public Type? AppbarRight { get; set; }
+	public Type? SidebarHeader { get; set; }
+	public Type? SidebarFooter { get; set; }
+}
---
+++ src/Sienar.Utils.Blazor/Infrastructure/IComponentProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Sienar.Infrastructure;
+
+/// <summary>
+/// A provider to contain references to various components to render in the Sienar UI
+/// </summary>
+public interface IComponentProvider
+{
+	/// <summary>
+	/// The default layout component to use when no layout is specified
+	/// </summary>
+	public Type? DefaultLayout { get; set; }
+
+	/// <summary>
+	/// A component to render in place of the default dashboard UI
+	/// </summary>
+	public Type? Dashboard { get; set; }
+
+	/// <summary>
+	/// A component to render on the left side of the appbar on large web screens
+	/// </summary>
+	public Type? AppbarLeft { get; set; }
+
+	/// <summary>
+	/// A component to render on the right side of the appbar on large web screens
+	/// </summary>
+	public Type? AppbarRight { get; set; }
+
+	/// <summary>
+	/// A component to render at the top of the sidebar on dashboard screens
+	/// </summary>
+	public Type? SidebarHeader { get; set; }
+
+	/// <summary>
+	/// A component to render at the bottom of the sidebar on dashboard screens
+	/// </summary>
+	public Type? SidebarFooter { get; set; }
+}
