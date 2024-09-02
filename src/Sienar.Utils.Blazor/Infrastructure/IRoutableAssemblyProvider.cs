5f4e4d50b2c330816d2abb91a793efd67c130f5c
Christian LeVesque <christian@levesque.dev>
2024-09-02 10:41:08 -0500

    added routable assembly provider to allow Blazor to know which assemblies contain routable components

---
+++ src/Sienar.Utils.Blazor/Infrastructure/IRoutableAssemblyProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace Sienar.Infrastructure;
+
+/// <summary>
+/// A container for assemblies containing routable Blazor components
+/// </summary>
+public interface IRoutableAssemblyProvider : IList<Assembly>;
---
+++ src/Sienar.Utils.Blazor/Infrastructure/RoutableAssemblyProvider.cs
@@ -0,0 +1,9 @@
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Sienar.Infrastructure;
+
+/// <exclude />
+public class RoutableAssemblyProvider : List<Assembly>, IRoutableAssemblyProvider;
