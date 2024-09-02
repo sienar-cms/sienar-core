7bbde79bd5cb5ff596c9cc8825655121e6a7bb77
Christian LeVesque <christian@levesque.dev>
2024-09-02 12:38:27 -0500

    added WASM plugin interface

---
+++ src/Sienar.WasmPlugin/Plugins/IWasmPlugin.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace Sienar.Plugins;
+
+/// <summary>
+/// A plugin for a Sienar WebAssembly client
+/// </summary>
+public interface IWasmPlugin
+{
+	/// <summary>
+	/// Plugin data for the current plugin
+	/// </summary>
+	PluginData PluginData { get; }
+
+	/// <summary>
+	/// Performs operations against the application's <see cref="WebAssemblyHostBuilder"/>
+	/// </summary>
+	/// <param name="builder">the application's underlying <see cref="WebAssemblyHostBuilder"/></param>
+	void SetupDependencies(WebAssemblyHostBuilder builder) {}
+
+	/// <summary>
+	/// Performs operations against the application's <see cref="WebAssemblyHost"/>
+	/// </summary>
+	/// <param name="app">the application's underlying <see cref="WebAssemblyHost"/></param>
+	void SetupApp(WebAssemblyHost app) {}
+}
