d359a79f271648490f6bd6c6a49e4e7414c407ff
Christian LeVesque <christian@levesque.dev>
2024-09-02 10:43:30 -0500

    added stub AuthenticationStateProvider

---
+++ src/Sienar.Utils.Blazor/Infrastructure/AuthStateProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Sienar.Infrastructure;
+
+/// <summary>
+/// A WASM-friendly implementation of <see cref="AuthenticationStateProvider"/>
+/// </summary>
+public class AuthStateProvider : AuthenticationStateProvider
+{
+	/// <ignore />
+	public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+	{
+		return CreateAuthenticationState();
+	}
+
+	private static AuthenticationState CreateAuthenticationState()
+	{
+		return new(new ClaimsPrincipal());
+	}
+}
