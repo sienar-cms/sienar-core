c5969e4b6b8d146de6070019b6c404200fff7b1f
Christian LeVesque <christian@levesque.dev>
2024-09-02 11:18:15 -0500

    added IServiceProvider extensions to configure the IRoutableAssemblyProvider and IComponentProvider

---
+++ src/Sienar.Utils.Blazor/Extensions/SienarUtilsBlazorServiceProviderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Sienar.Infrastructure;
+
+namespace Sienar.Extensions;
+
+/// <summary>
+/// Contains <see cref="IServiceProvider"/> extension methods for the <c>Sienar.Utils.Blazor</c> assembly
+/// </summary>
+public static class SienarUtilsBlazorServiceProviderExtensions
+{
+	/// <summary>
+	/// Configures the <see cref="IRoutableAssemblyProvider">routable assembly provider</see>
+	/// </summary>
+	/// <param name="self">the service provider</param>
+	/// <param name="configurer">an <see cref="Action{IRoutableAssemblyProvider}">action</see> that configures the routable assembly provider</param>
+	/// <returns>the service provider</returns>
+	public static IServiceProvider ConfigureRoutableAssemblies(
+		this IServiceProvider self,
+		Action<IRoutableAssemblyProvider> configurer)
+		=> self.Configure(configurer);
+
+	/// <summary>
+	/// Configures the <see cref="IComponentProvider">component provider</see>
+	/// </summary>
+	/// <param name="self">the service provider</param>
+	/// <param name="configurer">an <see cref="Action{IComponentProvider}">action</see> that configures the component provider</param>
+	/// <returns>the service provider</returns>
+	public static IServiceProvider ConfigureComponents(
+		this IServiceProvider self,
+		Action<IComponentProvider> configurer)
+		=> self.Configure(configurer);
+}
