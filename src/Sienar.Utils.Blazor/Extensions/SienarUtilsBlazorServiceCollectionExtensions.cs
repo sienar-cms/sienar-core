e06bc16384220d50d21839381baffcce91c87ece
Christian LeVesque <christian@levesque.dev>
2024-09-02 10:46:36 -0500

    added service collection extensions class with method to add Blazor utilities

---
+++ src/Sienar.Utils.Blazor/Extensions/SienarUtilsBlazorServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Sienar.Infrastructure;
+
+namespace Sienar.Extensions;
+
+/// <summary>
+/// Contains <see cref="IServiceCollection"/> extension methods for the <c>Sienar.Utils.Blazor</c> assembly
+/// </summary>
+public static class SienarUtilsBlazorServiceCollectionExtensions
+{
+	/// <summary>
+	/// Adds Blazor-specific services to the service collection
+	/// </summary>
+	/// <param name="self">the service collection</param>
+	/// <returns>the service collection</returns>
+	public static IServiceCollection AddSienarBlazorUtilities(this IServiceCollection self)
+	{
+		self.TryAddSingleton<IRoutableAssemblyProvider, RoutableAssemblyProvider>();
+		self.TryAddSingleton<IComponentProvider, ComponentProvider>();
+		self.TryAddSingleton<AuthenticationStateProvider, AuthStateProvider>();
+		self.TryAddSingleton<IUserAccessor, BlazorUserAccessor>();
+
+		return self;
+	}
+}
