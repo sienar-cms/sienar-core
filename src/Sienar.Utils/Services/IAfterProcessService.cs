25e6cf3b2278f35b66b8e71282a5075a4789374d
christianlevesque <christian@levesque.dev>
2024-10-19 13:03:44 -0500

    added IAfterProcessService to wrap around after hooks

---
+++ src/Sienar.Utils/Services/IAfterProcessService.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Sienar.Hooks;
+
+namespace Sienar.Services;
+
+/// <summary>
+/// Runs after hooks for a hookable request
+/// </summary>
+/// <typeparam name="T">the type of the request or entity</typeparam>
+// ReSharper disable once TypeParameterCanBeVariant
+public interface IAfterProcessService<T>
+{
+	/// <summary>
+	///  Runs all after hooks for a hookable request
+	/// </summary>
+	/// <param name="input">the request or entity</param>
+	/// <param name="action">the action type</param>
+	Task Run(
+		T input,
+		ActionType action);
+}
