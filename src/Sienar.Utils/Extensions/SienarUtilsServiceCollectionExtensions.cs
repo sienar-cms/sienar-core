b84520a65740f299da37fa275f18d9f79dde038d
christianlevesque <christian@levesque.dev>
2024-10-19 11:35:19 -0500

    added default implementation of IStateValidatorService

--- src/Sienar.Utils/Extensions/SienarUtilsServiceCollectionExtensions.cs
+++ src/Sienar.Utils/Extensions/SienarUtilsServiceCollectionExtensions.cs
@@ -32,6 +32,7 @@ public static class SienarUtilsServiceCollectionExtensions
 		self.TryAddScoped(typeof(IService<,>), typeof(Service<,>));
 		self.TryAddScoped(typeof(IResultService<>), typeof(ResultService<>));
 		self.TryAddScoped(typeof(IAccessValidatorService<>), typeof(AccessValidatorService<>));
+		self.TryAddScoped(typeof(IStateValidatorService<>), typeof(StateValidatorService<>));
 		self.TryAddSingleton<IDashboardProvider, DashboardProvider>();
 		self.TryAddScoped<IDashboardGenerator, DashboardGenerator>();
 		self.TryAddSingleton<IMenuProvider, MenuProvider>();
---
+++ src/Sienar.Utils/Services/StateValidatorService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Sienar.Data;
+using Sienar.Hooks;
+
+namespace Sienar.Services;
+
+/// <inheritdoc />
+public class StateValidatorService<T> : IStateValidatorService<T>
+{
+	private readonly IEnumerable<IStateValidator<T>> _validators;
+	private readonly ILogger<IStateValidatorService<T>> _logger;
+
+	/// <exclude />
+	public StateValidatorService(
+		IEnumerable<IStateValidator<T>> validators,
+		ILogger<IStateValidatorService<T>> logger)
+	{
+		_validators = validators;
+		_logger = logger;
+	}
+
+	/// <inheritdoc />
+	public async Task<OperationResult<bool>> Validate(
+		T input,
+		ActionType action)
+	{
+		var wasSuccessful = true;
+		string? validationMessage = null;
+
+		try
+		{
+			foreach (var validator in _validators)
+			{
+				if (await validator.Validate(input, action) != OperationStatus.Success) wasSuccessful = false;
+			}
+		}
+		catch (Exception e)
+		{
+			_logger.LogError(e, "One or more state validators failed to run");
+			wasSuccessful = false;
+			validationMessage = StatusMessages.Processes.InvalidState;
+		}
+
+		return new(
+			wasSuccessful ? OperationStatus.Success : OperationStatus.Unprocessable,
+			wasSuccessful,
+			validationMessage);
+	}
+}
