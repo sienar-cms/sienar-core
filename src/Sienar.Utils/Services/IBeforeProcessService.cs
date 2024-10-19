032d7f5a38f8f85a412637391ceec71843cc6060
christianlevesque <christian@levesque.dev>
2024-10-19 12:24:13 -0500

    added IBeforeProcessService to wrap around before hooks

---
+++ src/Sienar.Utils/Services/IBeforeProcessService.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Sienar.Data;
+using Sienar.Hooks;
+
+namespace Sienar.Services;
+
+/// <summary>
+/// Runs before hooks for a hookable request
+/// </summary>
+/// <typeparam name="T">the type of the request or entity</typeparam>
+// ReSharper disable once TypeParameterCanBeVariant
+public interface IBeforeProcessService<T>
+{
+	/// <summary>
+	/// Runs all before hooks for a hookable request
+	/// </summary>
+	/// <param name="input">the request or entity</param>
+	/// <param name="action">the action type</param>
+	/// <returns>an operation result representing whether the hooks allow the process to continue</returns>
+	Task<OperationResult<bool>> Run(
+		T input,
+		ActionType action);
+}
