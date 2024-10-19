4edb33f6900f1f99fdc96137a0e0a0bbdc3784b7
christianlevesque <christian@levesque.dev>
2024-10-19 13:50:55 -0500

    imported IBotDetector interface from plugin-cms

---
+++ src/Sienar.Utils/Infrastructure/IBotDetector.cs
@@ -0,0 +1,16 @@
+﻿using Sienar.Data;
+
+namespace Sienar.Infrastructure;
+
+/// <summary>
+/// (Hopefully) detects bots
+/// </summary>
+public interface IBotDetector
+{
+	/// <summary>
+	/// Determines whether a honeypot has caught a bot
+	/// </summary>
+	/// <param name="honeypot">The honeypot</param>
+	/// <returns>whether the honeypot caught a bot</returns>
+	bool IsSpambot(Honeypot honeypot);
+}
